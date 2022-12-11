d409d48e4588e3f6ed803469683245324f617bb0
Brooke Gizaw <108835504+Brooke-Gizaw@users.noreply.github.com>
2022-12-11 13:05:47 -0600

    Upadate
    10 times faster than the previous version (on average).

--- main.py
+++ main.py
@@ -5,72 +5,52 @@ import pygame.camera
 import pygame
 import os
 
-os.chdir("D:/Coding Projects/stayoutofmyroom/assets")
-list_of_files = os.listdir()
-for m in range(0, len(list_of_files)-1):
-    os.remove(list_of_files[m])
+os.chdir("/assets")
 counter3 = 0
 image = pygame.SurfaceType
-slope = []
+slope = 1
+start = 0
 
 
 def check_captures():
-    global counter3, slope
-    if counter3 >= 1:
-
-        counter = 0
-        counter2 = 0
+    global counter3, slope, start
+    if counter3 >= 100:
+        start = time.time()
 
         img = read_image(("pic" + str(counter3) + ".jpg"))
-        img2 = read_image(("pic" + str(counter3 - 1) + ".jpg"))
 
         d = img.numpy()
-        c = img2.numpy()
 
         size_x = 128
         size_y = 72
 
+        color1 = 0
+        total = 0
+        different = 0
+
         for a in range(0, (size_y - 1)):
             for b in range(0, (size_x - 1)):  # range(0, (x[2] - 1))
-                if (c[0][15 * a][15 * b] + 10 >= d[0][15 * a][15 * b] <= c[0][15 * a][15 * b] - 10) and \
-                        (c[1][15 * a][15 * b] + 10 >= d[1][15 * a][15 * b] <= c[1][15 * a][15 * b] - 10) and \
-                        (c[2][15 * a][15 * b] + 10 >= d[2][15 * a][15 * b] <= c[2][15 * a][15 * b] - 10):
-                    pass
+                color2 = color1
+                color1 = 0.2989 * d[0][15 * a][15 * b] + 0.5870 * d[1][15 * a][15 * b] + 0.1140 * d[2][15 * a][15 * b]
+                if color2 + 20 >= color1 >= color2 - 20:
+                    total += 1
                 else:
-                    counter += 1
-                counter2 += 1
-                #  print(c[0][6 * a][6 * b], d[0][6 * a][6 * b])
+                    different += 1
+                    total += 1
 
-        e = counter/counter2
+        slope = (slope * 1.1 + ((different/total)*10))/11
 
-        #  print(e)
-
-        os.remove(("pic" + str(counter3 - 1) + ".jpg"))
-
-        if len(slope) == 6:
-            del slope[0]
-            slope.append(e)
-        else:
-            slope.append(e)
-
-        slope2 = (sum(slope)/len(slope)) - 0.005
-
-        # print("slope2:", slope2)
-
-        if e < slope2 and counter3 != 3:
+        if slope <= different/total:
             save_img_to_caught()
-
-        return e
-    else:
-        return "none"
 
 
 def capture_img_():
-    print("started")
     pygame.camera.init(None)
     camlist = pygame.camera.list_cameras()
     cam = pygame.camera.Camera(camlist[0], (1920, 1080), "rgb")
     cam.start()
+    time.sleep(2)
+    print("started")
 
     while True:
         global counter3, image
@@ -88,12 +68,11 @@ def save_img_to_caught():
         return
     print("caught")
     timelist_ = time.localtime()
-    os.chdir("D:/Coding Projects/stayoutofmyroom/caught")
+    os.chdir("/caught")
     pygame.image.save(image, (str(timelist_[0]) + "__" + str(timelist_[1]) + "__" + str(timelist_[2]) + "__" +
                               str(timelist_[3]) + "_" + str(timelist_[4]) + "_" + str(timelist_[5]) + ".jpg"))
-    name = f"{timelist_[0]}__{str(timelist_[1])}__{str(timelist_[2])}__{str(timelist_[3])}_{str(timelist_[4])}_{str(timelist_[5])}.jpg"
-    os.chdir("D:/Coding Projects/stayoutofmyroom/assets")
+    os.chdir("/assets")
+    print("time:", abs(time.time() - start))
 
 
-time.sleep(10)
 capture_img_()
